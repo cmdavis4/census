98cae4bff486d56e47f481b77d1881061d5f384a
Charles Davis <charles@twosixcapital.com>
2017-02-05 23:30:32 -0800

    Added function to add percentile for a given metric

--- analysis.py
+++ analysis.py
@@ -1,8 +1,11 @@
 import pandas as pd
 import numpy as np
 
-def add_quantiles(df, metric, weight='marsupwt'):
-    df = df.sort_values(metric)
-    samp_prob = df[weight]/df[weight].sum()
-    df['{}_quantile'.format(metric)] = np.cumsum(samp_prob) / samp_prob.sum()
+def add_quantiles(df, metrics, weight='marsupwt'):
+    if isinstance(metrics, str):
+        metrics = [metrics]
+    for metric in metrics:
+        df = df.sort_values(metric)
+        samp_prob = df[weight]/df[weight].sum()
+        df['{}_quantile'.format(metric)] = np.cumsum(samp_prob) / samp_prob.sum()
     return df
